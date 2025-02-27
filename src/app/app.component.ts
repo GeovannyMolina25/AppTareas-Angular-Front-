525c0e5109fc8934cfe47eb95016366e620b9003
Nelson Molina <geovannysangucho25@gmail.com>
2025-02-26 21:56:32 -0500

    Correccion de rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Child2Component } from './components/child2/child2.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, ReactiveFormsModule, ChildComponent, Child2Component], 
+  imports: [RouterModule, ReactiveFormsModule], 
   templateUrl:'./app.component.html',
   styleUrl:'./app.component.css'
 })
