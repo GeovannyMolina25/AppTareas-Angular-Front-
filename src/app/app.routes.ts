45b9d764db9faab6a5e6b13636a97bd5688623b7
Nelson Molina <geovannysangucho25@gmail.com>
2025-02-26 21:51:40 -0500

    Crwacion de componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,5 @@ import { Routes } from '@angular/router';
 
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },  // Redirige a /login si está vacío
-  { path: '**', redirectTo: 'login' }  // Redirige a login para rutas no definidas
+  {path: '', component: HomeComponent}
 ];
